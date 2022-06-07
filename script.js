d8651a013cd99b3b41f8b5f24bc5104aaa2b1e15
Correa Alejandro Joel <joelcorrea.alejandro123@gmail.com>
2022-06-07 15:44:21 -0300

    validaciones completas con javascript

--- script.js
+++ script.js
@@ -2,16 +2,136 @@
 
 
 const inputNombre = document.getElementById("nombre");
+const inputApellido = document.getElementById("apellido");
+const inputEmail = document.getElementById("email");
+const inputEdad = document.getElementById("edad");
+const inputSexos = document.querySelectorAll("input[type='radio']");
+const inputIntereses = document.querySelectorAll("input[type='checkbox']");
+const buttonEnviar = document.querySelector("button");
 const mensajesErrores = document.querySelectorAll(".mensajeError");
-inputNombre.addEventListener("blur" , function(){
+console.log(mensajesErrores);
+
+function validarNombre(){
     if( inputNombre.value.length < 3){
         mensajesErrores[0].classList.remove("hidden")
         inputNombre.classList.add("borderError");
         inputNombre.classList.remove("borderCorrecto");
+        return false;
     }
     else{
         mensajesErrores[0].classList.add("hidden")
         inputNombre.classList.remove("borderError");
         inputNombre.classList.add("borderCorrecto");
+        return true;
+    }
+}
+function validarApellido(){
+    if( inputApellido.value.length < 3){
+        mensajesErrores[1].classList.remove("hidden")
+        inputApellido.classList.add("borderError");
+        inputApellido.classList.remove("borderCorrecto");
+        return false;
+    }
+    else{
+        mensajesErrores[1].classList.add("hidden")
+        inputApellido.classList.remove("borderError");
+        inputApellido.classList.add("borderCorrecto");
+        return true;
+    }
+}
+function validarEmail(){
+    if(! inputEmail.value.match(/[a-z0-9]+@[a-z]+\.[a-z]{2,3}/)){
+        mensajesErrores[2].classList.remove("hidden")
+        inputEmail.classList.add("borderError");
+        inputEmail.classList.remove("borderCorrecto");
+        return false;
+    }
+    else{
+        mensajesErrores[2].classList.add("hidden")
+        inputEmail.classList.remove("borderError");
+        inputEmail.classList.add("borderCorrecto");
+        return true;
+    }
+}
+function validarEdad(){
+    if(!isNaN(inputEdad.value) && inputEdad.value != ""){
+        if(inputEdad.value >= 0 && inputEdad.value <= 100){
+            mensajesErrores[3].classList.add("hidden")
+            inputEdad.classList.remove("borderError");
+            inputEdad.classList.add("borderCorrecto");
+            return true;
+        }
+        else{
+            mensajesErrores[3].classList.remove("hidden")
+            inputEdad.classList.add("borderError");
+            inputEdad.classList.remove("borderCorrecto");
+            return false;
+        }
+    }
+    else{
+        mensajesErrores[3].classList.remove("hidden")
+        inputEdad.classList.add("borderError");
+        inputEdad.classList.remove("borderCorrecto");
+        return false;
+    }
+}
+function validarIntereses(){
+    valido = false
+    inputIntereses.forEach(function(elemento){
+        if(elemento.checked){
+            valido = true;
+        }
+    })
+    if(valido){
+        mensajesErrores[5].classList.add("hidden")
+        return true;
+    }
+    else{
+        mensajesErrores[5].classList.remove("hidden")
+        return false;
+    }
+}
+
+
+function validarCampos(){
+    if(validarNombre()){
+        if(validarApellido()){
+            if(validarEmail()){
+                if(validarEdad()){
+                   if(validarIntereses()){
+                        return true;
+                   }
+                }
+                else{
+                    inputEdad.focus()
+                }
+            }
+            else{
+                inputEmail.focus();
+            }
+        }
+        else{
+            inputApellido.focus();
+        }
+    }
+    else{
+        inputNombre.focus();
+    }
+    return false;
+}
+
+inputNombre.addEventListener("blur" , validarNombre)
+inputApellido.addEventListener("blur" , validarApellido)
+inputEmail.addEventListener("blur" , validarEmail)
+inputEdad.addEventListener("blur" , validarEdad)
+
+buttonEnviar.addEventListener("click" , function(e){
+    if(validarCampos()){
+        alert("datos correctos")
+        e.preventDefault();
+    }
+    else{
+        alert("datos faltantes")
+        e.preventDefault();
     }
 })
